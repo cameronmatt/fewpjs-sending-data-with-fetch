959448baae19f6748d2df2037da44700cd59cfef
Matthew <cameron.matt@outlook.com>
2021-03-28 00:28:29 +1100

    Sending Data with fetch done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email
+        }) //converts newUser object to text to be sent
+        })
+        .then(function(response){
+            return response.json(); //handles resolution of the promise
+        })
+        .then(function(data){
+            document.body.innerHTML=data['id']
+        })
+        .catch(function(error){ //catches any possible errors and displays in console log
+            document.body.innerHTML=error.message;
+        })
+    }
+
